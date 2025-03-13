a18f248970e6f3495a05db93b9733605c445362d
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-13 10:01:05 -0300

    Par e Ímpar.cs

---
+++ Par e Ímpar.cs
@@ -0,0 +1,20 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+    int num, resto;
+    Console.Write("digita ai amig: ");
+    num = int.Parse(Console.ReadLine());
+    resto = num % 2;
+    if (resto = = 0)
+    Console.WriteLine("Par");
+    else
+    Console.WriteLine("Impar");
+    }
+    
+}
