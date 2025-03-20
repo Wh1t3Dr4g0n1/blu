f2575c2be0bb32cf01d00bcdef25c4fc2f2ed1f5
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-19 21:47:31 -0300

    3 tipos de triangulo.cs

---
+++ 3 tipos de triangulo.cs
@@ -0,0 +1,14 @@
+// data 19/03/2025
+// 1.  inicio
+// 2. var a,b,c
+// 3. Leia a,b,c
+// 4. Se (a = b e a = c e b = c)
+//    Mostre "equilátero"
+//	Senão Se a ≠ b e a ≠ c e b ≠ c)
+//	   Mostre "Escaleto"
+//	Senão
+//	   Mostre "Isóceles"
+//	   Fim-se
+//	   Fim-se
+// 5. Fim
+
