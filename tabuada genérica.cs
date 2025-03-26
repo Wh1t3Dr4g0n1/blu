533c8839304374fe22bce121850eddd000b037d8
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-26 19:54:16 -0300

    tabuada genérica.cs

---
+++ tabuada genérica.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meu_lindo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tabuada genérica
+
+            Console.Write("manda um numero para o teu pai, que eu lhe darei a tabuada completa do mesmo: ");
+            int blu;
+            int bla;
+            blu = Convert.ToInt32(Console.ReadLine());
+            for (bla = 1; bla <= 10; bla++)
+            {
+                Console.ReadKey();
+                Console.WriteLine(bla + " x " + blu + " = " + (blu * bla));
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
