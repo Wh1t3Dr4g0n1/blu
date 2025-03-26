0baaa265093c0bb6abed5c9da26867d017ceabcd
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-26 20:54:36 -0300

    de agora.cs

---
+++ de agora.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meu_lindo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int blu;
+            int bla;
+            int resto;
+            Console.WriteLine("lhe darei uma quantia de numeros pares para você de acordo com o limite que você me der");
+            Console.Write("inicio do limite: ");
+            bla = Convert.ToInt32(Console.ReadLine());
+            Console.Write("fim do limite: ");
+            blu = Convert.ToInt32(Console.ReadLine());
+            resto == blu % 2;
+            if (resto == 0)
+
+            else
+            Console.WriteLine("esse numero n é par");
+            
+        }
+    }
+}
