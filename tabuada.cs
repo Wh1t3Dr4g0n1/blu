5e539253387e03b0b046ec090b9f139be864cea3
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-26 19:42:18 -0300

    tabuada.cs

---
+++ tabuada.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for ( a = 1; a <= 10; c++)
+            {
+                Console.ReadKey();
+                Console.WriteLine("7x " + a + "=" + (7 * a));
+            }
+            Console.ReadKey();
+        }
+    }
+}
