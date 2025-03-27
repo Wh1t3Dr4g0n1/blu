9ad3fa19d0b38a41906b4c90e599e1dd3d0eefa8
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-26 22:29:24 -0300

    Calcular Salário.cs

---
+++ Calcular Salário.cs
@@ -0,0 +1,48 @@
+// Crie um progama que solicite do usu[ariio os seguintes dados:
+// - nome
+// - rg
+// - cpf
+// - idade
+// - sexo
+// - estado civil
+// - salario bruto
+//
+// Calcule:
+// - Desconto de 11% de INSS
+// - Desconto de 5% de Conv. Médico
+// - Desconto de Vale Transporte
+// - Salário Línquido
+//
+// Obs: Se o funcinário do sexo "Masculino" 
+// acrescente 1000,00 no Salário Línquido, caso 
+// contrário acrescente 800,00 no Salário Línquido.
+//
+// Algoritmo
+//
+// 1. Início
+// 2. var nome,rg, cpf, EC (estado civil), sexo: carater
+//    var idade: inteiro
+//    var SB (Salário Bruto),INSS, CM (Conv. Médico), VT (Vale Transporte), SL (Salário Línquido): real
+// 3. Leia nome
+// 4. Leia  rg
+// 5. Leia cpf
+// 6. Leia idade
+// 7. Leia EC
+// 8. Leia sexo
+// 9. Leia SB
+// 10. INSS = SB * 0.11
+// 11. Mostre INSS
+// 12. CM = SB * 0.06
+// 13. Mostre CM
+// 14. VT = SB * 0.06
+// 15. Mostre VT
+// 16. SL = SB - (INSS+CM+VT)
+// 17. Mostre SL
+// 18. Se (sexo = "M")
+//        SL = SL + 1000.00
+//  	Senão
+//	      SL = SL + 800.00
+
+
+                                    
+	   
