6ed7eaaa405d831399cd3c0a52712c9e0f9fbd87
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-03-27 15:36:40 -0300

    Calcular Salário.cs

--- Calcular Salário.cs
+++ Calcular Salário.cs
@@ -45,103 +45,6 @@
 
 
 
-// Crie um progama que solicite do usuario os seguintes dados:
-// - nome
-// - rg
-// - cpf
-// - idade
-// - sexo
-// - estado civil
-// - salario bruto
-//
-// Calcule:
-// - Desconto de 11% de INSS
-// - Desconto de 5% de Conv. Médico
-// - Desconto de Vale Transporte
-// - Salário Línquido
-//
-// Obs: Se o funcinário do sexo "Masculino" 
-// acrescente 1000,00 no Salário Línquido, caso 
-// contrário acrescente 800,00 no Salário Línquido.
-//
-// Algoritmo
-//
-// 1. Início
-// 2. var nome,rg, cpf, EC (estado civil), sexo: carater
-//    var idade: inteiro
-//    var SB (Salário Bruto),INSS, CM (Conv. Médico), VT (Vale Transporte), SL (Salário Línquido): real
-// 3. Leia nome
-// 4. Leia  rg
-// 5. Leia cpf
-// 6. Leia idade
-// 7. Leia EC
-// 8. Leia sexo
-// 9. Leia SB
-// 10. INSS = SB * 0.11
-// 11. Mostre INSS
-// 12. CM = SB * 0.06
-// 13. Mostre CM
-// 14. VT = SB * 0.06
-// 15. Mostre VT
-// 16. SL = SB - (INSS+CM+VT)
-// 17. Mostre SL
-// 18. Se (sexo = "M")
-//        SL = SL + 1000.00
-//  	Senão
-//	      SL = SL + 800.00
-
-
-
-
-
-
-// Crie um progama que solicite do usuario os seguintes dados:
-// - nome
-// - rg
-// - cpf
-// - idade
-// - sexo
-// - estado civil
-// - salario bruto
-//
-// Calcule:
-// - Desconto de 11% de INSS
-// - Desconto de 5% de Conv. Médico
-// - Desconto de Vale Transporte
-// - Salário Línquido
-//
-// Obs: Se o funcinário do sexo "Masculino" 
-// acrescente 1000,00 no Salário Línquido, caso 
-// contrário acrescente 800,00 no Salário Línquido.
-//
-// Algoritmo
-//
-// 1. Início
-// 2. var nome,rg, cpf, EC (estado civil), sexo: carater
-//    var idade: inteiro
-//    var SB (Salário Bruto),INSS, CM (Conv. Médico), VT (Vale Transporte), SL (Salário Línquido): real
-// 3. Leia nome
-// 4. Leia  rg
-// 5. Leia cpf
-// 6. Leia idade
-// 7. Leia EC
-// 8. Leia sexo
-// 9. Leia SB
-// 10. INSS = SB * 0.11
-// 11. Mostre INSS
-// 12. CM = SB * 0.06
-// 13. Mostre CM
-// 14. VT = SB * 0.06
-// 15. Mostre VT
-// 16. SL = SB - (INSS+CM+VT)
-// 17. Mostre SL
-// 18. Se (sexo = "M")
-//        SL = SL + 1000.00
-//  	Senão
-//	      SL = SL + 800.00
-
-
-
 
 
 
@@ -167,9 +70,9 @@ namespace meu_lindo
             do
             {
                 Console.Clear();
-                Console.WriteLine("==========================");
+                Console.WriteLine("=====================================================================");
                 Console.WriteLine("Porfavor, precisarei de alguns dados seus para calcular o seu salário");
-                Console.WriteLine("==========================");
+                Console.WriteLine("=====================================================================");
                 Console.Write("Seu nome: ");
                 nome = Console.ReadLine();
                 Console.Write("Seu RG: ");
@@ -196,6 +99,7 @@ namespace meu_lindo
                     SL = SL + 800.00;
 
                 Console.Clear();
+                Console.WriteLine("========================");
                 Console.WriteLine("Nome: " + nome);
                 Console.WriteLine("RG: " + rg);
                 Console.WriteLine("CPF: " + cpf);
@@ -207,7 +111,7 @@ namespace meu_lindo
                 Console.WriteLine("Seu Convenio Médico: " + CM);
                 Console.WriteLine("Seu Vale Transporte: " + VT);
                 Console.WriteLine("Seu Salário: " + SL);
-                Console.WriteLine("==========================");
+                Console.WriteLine("========================");
                 Console.WriteLine("Quer recomeçar(S/N)?: ");
                 cont = Console.ReadLine();
             }
@@ -216,3 +120,8 @@ namespace meu_lindo
         }
     }
 }
+
+
+
+
+
