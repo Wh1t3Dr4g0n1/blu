36d76bb88d2ae35e30d2ca4736da06153e12c36f
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-04-02 20:30:39 -0300

    produto.cs

---
+++ produto.cs
@@ -0,0 +1,11 @@
+// 1. ínicio
+// 2. var: string nome 
+// 3. var: double preço
+// 4. Mostrar "dê o nome do produto e o valor"
+// 5. var: double desconto
+// 6. desconto = Preço * 0.10
+// 7. var: double acrescimo
+// 8. Se for parcelado em 2x
+// 9. acrescimo = preço * 0.02
+// 10. Se for parcelado em 3x a 10x
+// 11. acrescimo = preço * 0.05
