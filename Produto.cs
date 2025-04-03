f5f0100f89a3063e8df033f2564ed724d811a6ee
Wh1t3Dr4g0n <jonatanmmribeiro@gmail.com>
2025-04-02 21:28:03 -0300

    Produto.cs

--- Produto.cs
+++ Produto.cs
@@ -26,6 +26,22 @@
 
 
 
+// 1. ínicio
+// 2. var: string nome 
+// 3. var: double preço
+// 4. Mostrar "dê o nome do produto e o valor"
+// 5. var: double desconto
+// 6. desconto = Preço * 0.10
+// 7. var: double acrescimo
+// 8. Se for parcelado em 2x
+// 9. acrescimo = preço * 0.02
+// 10. Se for parcelado em 3x a 10x
+// 11. acrescimo = preço * 0.05
+
+
+
+
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
@@ -61,7 +77,7 @@ namespace meu_lindo
                 {
                     Console.Clear();
                     precoa = preco * 0.10;
-                    total = precoa - preco;
+                    total = preco - precoa;
                     Console.WriteLine("========================");
                     Console.WriteLine("Pagamento a vista.");
                     Console.WriteLine("Seu Produto: " + produto);
